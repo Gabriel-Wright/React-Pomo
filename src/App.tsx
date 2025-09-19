d642af3be9c1edc4a9b361a3c6bfdd0fb842df2f
Gabriel <gabriel.magnus.wright@gmail.com>
2025-09-19 16:55:40 +0100

    more fenagling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
-import Button from "./components/Button";
 function App() {
   return (
-    <Button
-      buttonName="test-button"
-      buttonAlert="button-alert"
-      buttonFunction={(alert: string) => console.log("clicked " + alert)}
-    ></Button>
+    <div>
+      <h1>test</h1>
+    </div>
   );
 }
 
